2202cfa6cac054505741a1c65dd467cb80c3b56d
constf1 <60385526+constf1@users.noreply.github.com>
2020-01-28 13:48:08 +0300

    + a div for result

--- js/main.js
+++ js/main.js
@@ -10,9 +10,17 @@ const letters = [
   'IN', 'AP', 'ASZ', 'AQ', 'AR',
   'ASH', 'AT', 'DUSHENNA', 'KAD', 'AIN'];
 
+const result = document.createElement('div');
+result.className = 'result';
+
 const keyboard = document.getElementById('MANDAIC_KEYBOARD');
+
+// Append letters
 letters.forEach((letter, index) => {
   const btn = document.createElement('button');
   btn.innerText = letter;
   keyboard.appendChild(btn);
 });
+
+// Append result
+keyboard.appendChild(result);
