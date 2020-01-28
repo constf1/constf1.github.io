25d59b29905bf466d8e319613d756334cb27ce83
constf1 <60385526+constf1@users.noreply.github.com>
2020-01-28 13:35:04 +0300

    keyboard prototype

--- js/main.js
+++ js/main.js
@@ -9,3 +9,10 @@ const letters = [
   'AK', 'AL', 'AM', 'AN', 'AS',
   'IN', 'AP', 'ASZ', 'AQ', 'AR',
   'ASH', 'AT', 'DUSHENNA', 'KAD', 'AIN'];
+
+const keyboard = document.getElementById('MANDAIC_KEYBOARD');
+letters.forEach((letter, index) => {
+  const btn = document.createElement('button');
+  btn.innerText = letter;
+  keyboard.appendChild(btn);
+});
